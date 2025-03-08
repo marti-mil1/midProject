43a1d6d07454a04110aa7398d4a8fed3171359d9
Martina Milone <martina_milone@icloud.com>
2025-03-09 00:12:07 +0100

    Add hero section, project section, testimonial section, services section for mobile.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,19 @@
 let burgerMenu = document.querySelector('.burger-menu');
 let burgerIcon = document.querySelector('.burger-img');
 let closeIcon = document.querySelector('.close-img')
-let navbar = document.querySelector('.navbar');
+let navbarMobile = document.querySelector('.navbar-mobile');
 
 burgerMenu.addEventListener('click', () => {
-    if (navbar.style.display === 'block') {
-        navbar.style.display = 'none';
+    if (navbarMobile.style.display === 'block') {
+        /* HIDDEN CLOSED MENU */
+        navbarMobile.style.display = 'none';
         burgerIcon.style.display = 'block';
-        closeIcon.style.display ='none';
+        closeIcon.style.display = 'none';
     } else {
-        navbar.style.display = 'block';
+        /* VISIBLE MENU */
+        navbarMobile.style.display = 'block';
         burgerIcon.style.display = 'none';
         closeIcon.style.display = 'block';
     }
 });
+
