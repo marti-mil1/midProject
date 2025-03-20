1e595499b5862068496c72664fdde93ae137e774
Martina Milone <martina_milone@icloud.com>
2025-03-20 20:26:31 +0100

    Move index.html, index.css, index.js to root. Update all the anchors in all the pages

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ let burgerMenu = document.querySelector('.burger-menu');
 let burgerIcon = document.querySelector('.burger-img');
 let closeIcon = document.querySelector('.close-img')
 let navbarMobile = document.querySelector('.navbar-mobile');
+let body = document.querySelector('body');
 
 burgerMenu.addEventListener('click', () => {
     if (navbarMobile.style.display === 'block') {
         /* HIDDEN CLOSED MENU */
         navbarMobile.style.display = 'none';
+        body.style.overflowY = 'visible';
         burgerIcon.style.display = 'block';
         closeIcon.style.display = 'none';
     } else {
         /* VISIBLE MENU */
         navbarMobile.style.display = 'block';
+        body.style.overflowY = 'hidden';
         burgerIcon.style.display = 'none';
         closeIcon.style.display = 'block';
     }
